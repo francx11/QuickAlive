e6a0584f7267a06aca0d795cda8d67603d29d52f
francx11 <101357258+francx11@users.noreply.github.com>
2024-02-01 17:49:14 +0100

    Instalando Twig y configurando la base de datos

---
+++ backend/admin/prueba.php
@@ -0,0 +1,17 @@
+<?php
+require_once "../bd/bd.php";
+
+$bd = new BD();
+
+// Obtén la conexión
+$conexion = $bd->getConexion();
+
+// Comprueba si la conexión se realizó correctamente
+if ($conexion->connect_errno) {
+    echo "Fallo en la conexión: " . $conexion->connect_errno;
+} else {
+    echo "Conexión exitosa a la base de datos.";
+}
+
+// Cierra la conexión
+$bd->cerrarConexion();

5c99e2c94103c6cc49568ffcc40d45eb8e236cf8
francx11 <franexca@gmail.com>
2024-04-17 13:35:33 +0200

    Añadiendo funcionalidad de alta de actividad con varios tipos de categorías

---
+++ backend/admin/gestionActividades/renderAltaActividad.php
@@ -0,0 +1,27 @@
+<?php
+// Incluir el archivo que contiene la lógica para la conexión a la base de datos
+require_once "../../bd/bd.php";
+// Incluir el archivo autoload.php que probablemente es parte de la configuración de Composer
+require "../../../vendor/autoload.php";
+
+// Iniciar una sesión PHP para mantener y acceder a variables de sesión
+session_start();
+
+// Configurar Twig para cargar plantillas desde el directorio especificado
+$loader = new \Twig\Loader\FilesystemLoader('../../../frontend/admin/templates/gestionActividades');
+$twig = new \Twig\Environment($loader);
+
+// Crear una instancia de la clase BD para interactuar con la base de datos
+$bd = new BD();
+
+// Verificar si el usuario está registrado como 'root' y está logueado
+$registradoRoot = isset($_SESSION['loggedin']) && isset($_SESSION['rol']) && $_SESSION['loggedin'] && $_SESSION['rol'] == 'root';
+// Obtener el estado de inicio de sesión del usuario
+$logueado = $_SESSION['loggedin'];
+
+// Si el usuario está registrado como 'root'
+if ($registradoRoot) {
+
+    $tiposPreferencias = $bd->getAllTipoPreferencias();
+    echo $twig->render('altaActividad.html', ['logueado' => $logueado, 'tiposPreferencias' => $tiposPreferencias]);
+}

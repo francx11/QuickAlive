49291dce5bfd127ccf1200a20413474e536da9f0
francx11 <franexca@gmail.com>
2024-04-18 12:58:45 +0200

    Funcionalidad de modificarActividad realizada

--- backend/admin/gestionActividades/modificarActividad.php
+++ backend/admin/gestionActividades/modificarActividad.php
@@ -6,15 +6,8 @@ require "../../../vendor/autoload.php";
 // Inicia la sesión
 session_start();
 
-// Configura el cargador y el entorno de Twig para las plantillas
-$loader = new \Twig\Loader\FilesystemLoader('../../../frontend/admin/templates/gestionActividades');
-$twig = new \Twig\Environment($loader);
-
 // Crea una instancia de la clase BD para interactuar con la base de datos
 $bd = new BD();
-
-// Obtiene el ID de la actividad de la solicitud GET, o establece -1 si no se proporciona
-$idActividad = isset($_GET['id']) ? $_GET['id'] : -1;
 
 // Verifica si el usuario está autenticado como root
 $registradoRoot = isset($_SESSION['loggedin']) && isset($_SESSION['rol']) && $_SESSION['loggedin'] && $_SESSION['rol'] == 'root';
@@ -22,48 +15,51 @@ $registradoRoot = isset($_SESSION['loggedin']) && isset($_SESSION['rol']) && $_S
 // Si el usuario es root, procede con la modificación de la actividad
 if ($registradoRoot) {
 
+    // Obtiene el ID de la actividad de la solicitud POST, o establece -1 si no se proporciona
+    $idActividad = isset($_POST['idActividad']) ? $_POST['idActividad'] : -1;
+
     // Obtiene los datos de la actividad original
     $actividadOriginal = $bd->getActividad($idActividad);
-
-    // Obtiene la galería de imágenes actual de la actividad
-    $galeriaActual = $bd->getGaleriaActividad($idActividad);
 
     // Si se ha enviado una solicitud POST, procesa la modificación de la actividad
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         // Verifica si se han proporcionado los datos básicos de la actividad en el formulario
-        $datosBasicosCorrectos = isset($_POST['nombreActividad'], $_POST['descripcion'], $_POST['tipoActividad'], $_POST['subtipoActividad'], $_POST['duracion']);
+        $datosBasicosCorrectos = isset($_POST['nombre'], $_POST['descripcion'], $_POST['duracion']);
 
         // Si los datos básicos son correctos, procede con la modificación
         if ($datosBasicosCorrectos) {
             // Obtiene los datos básicos de la actividad del formulario
-            $nombreActividad = $_POST['nombreActividad'];
+            $nombreActividad = $_POST['nombre'];
             $descripcion = $_POST['descripcion'];
-            $tipoActividad = $_POST['tipoActividad'];
-            $subTipoActividad = $_POST['subtipoActividad'];
             $duracion = $_POST['duracion'];
 
             // Verifica si se están modificando los campos y establece valores predeterminados si es necesario
             $nombreActividad = ($nombreActividad != '') ? $nombreActividad : $actividadOriginal->getNombreActividad();
             $descripcion = ($descripcion != '') ? $descripcion : $actividadOriginal->getDescripcion();
-            $tipoActividad = ($tipoActividad != '') ? $tipoActividad : $actividadOriginal->getTipoActividad();
-            $subTipoActividad = ($subTipoActividad != '') ? $subTipoActividad : $actividadOriginal->getSubTipoActividad();
             $duracion = ($duracion != '') ? $duracion : $actividadOriginal->getDuracion();
 
             // Intenta modificar la actividad en la base de datos
-            if ($bd->modificarActividad($idActividad, $nombreActividad, $descripcion, $tipoActividad, $subTipoActividad, $duracion)) {
+            if ($bd->modificarActividad($idActividad, $nombreActividad, $descripcion, $duracion)) {
                 echo 'Datos básicos modificados con éxito';
             } else {
                 echo 'Error en modificación de datos básicos';
             }
-
-            // Redirige de vuelta a la página de gestión de actividades
-            header("Location: gestionActividades.php");
         } else {
             echo 'Error en POST de algún dato básico';
         }
 
+        if (isset($_POST['preferencias'])) {
+            // Decodificar las preferencias desde el JSON
+            $categoriasJSON = $_POST['preferencias'];
+            $categorias = json_decode($categoriasJSON, true);
+
+            if ($bd->modificarTipoPreferencias($idActividad, $categorias)) {
+                echo 'Modificadas correctamente las categorías';
+            }
+        }
+
         // Procesa la carga de nuevas imágenes para la galería de la actividad, si se han proporcionado
-        if ($_FILES['imagenes']) {
+        if (isset($_FILES['imagenes'])) {
             // Obtiene las imágenes subidas del formulario
             $imagenesPOST = $_FILES['imagenes']['tmp_name'];
 
@@ -89,15 +85,9 @@ if ($registradoRoot) {
                         }
                     }
                 }
-
-                // Redirige de vuelta a la página de gestión de actividades
-                header("Location: gestionActividades.php");
             } else {
                 echo "ID de actividad erróneo.";
             }
         }
     }
-
-    // Renderiza la plantilla Twig 'modificarActividad.html', pasando el ID de la actividad y las imágenes de la galería actual
-    echo $twig->render('modificarActividad.html', ['idActividad' => $idActividad, 'imagenes' => $galeriaActual]);
 }

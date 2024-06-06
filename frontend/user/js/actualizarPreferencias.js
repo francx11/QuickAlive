43dd6416833ad31a5e407db2bc21061bdcefdd90
francx11 <franexca@gmail.com>
2024-06-06 21:19:07 +0200

    Diseño de: Recuperación de contraseña, actualización de preferencias, lista de actividades; y aplicación de fuente Roboto Salb

--- frontend/user/js/actualizarPreferencias.js
+++ frontend/user/js/actualizarPreferencias.js
@@ -1,51 +1,32 @@
 $(document).ready(function() {
-    // Función para alternar la selección de preferencias al hacer clic
-    $(".preferencia").click(function() {
-        $(this).toggleClass("seleccionada"); // Alternar la clase "seleccionada" al hacer clic
+    $('.preference').on('click', function() {
+        $(this).toggleClass('selected');
     });
 
-    // Evento click para el botón de guardar preferencias
-    $("#actualizarPreferencias").click(function() {
-        // Obtener el ID del usuario del atributo de datos del elemento HTML
-        var idUsuario = $("#usuario").data("id-usuario");
+    $('#actualizarPreferencias').on('click', function() {
+        const selectedPreferences = $('.preference.selected').map(function() {
+            return {
+                idTipoPreferencia: $(this).data('id'),
+                nombreTipoPreferencia: $(this).data('nombre')
+            };
+        }).get();
 
-        console.log(idUsuario);
+        const userId = $('#usuario').data('id-usuario');
 
-        // Array para almacenar las preferencias seleccionadas
-        var preferenciasSeleccionadas = [];
+        console.log(selectedPreferences);
 
-        
-        // Recorrer todos los checkboxes de preferencias marcados
-        $("input[name='preferencias[]']:checked").each(function() {
-        // Obtener el nombreTipo y el idTipoPreferencia de cada checkbox
-        var nombreTipoPreferencia = $(this).data("nombre");
-        var idTipoPreferencia = $(this).val();
-
-        // Agregar los datos al array de preferenciasSeleccionadas
-        preferenciasSeleccionadas.push({
-            nombreTipoPreferencia: nombreTipoPreferencia,
-            idTipoPreferencia: idTipoPreferencia
-        });
-        });
-
-        console.log(preferenciasSeleccionadas);
-
-        // Realizar la solicitud AJAX para enviar las preferencias seleccionadas y el ID del usuario al archivo PHP
         $.ajax({
-            url: "actualizarPreferencias.php",
-            type: "POST",
-            data: { idUsuario: idUsuario, preferencias: preferenciasSeleccionadas },
+            type: 'POST',
+            url: 'actualizarPreferencias.php',
+            data: {
+                idUsuario: userId,
+                preferencias: selectedPreferences
+            },
             success: function(response) {
-                // Manejar la respuesta del servidor si es necesario
-                console.log("Preferencias guardadas exitosamente");
-                //window.location.href = '../../../backend/api/sesiones/login.php';
-                //window.location.href = '../../../backend/user/gestionRegistro/registroPreferencias.php'
-                // Manejar la respuesta del servidor si es necesario
                 window.location.href = '../../../backend/user/pantallaInicial.php';
             },
-            error: function(xhr, status, error) {
-                // Manejar errores de la solicitud AJAX si es necesario
-                console.error("Error al guardar las preferencias:", error);
+            error: function() {
+                alert('Error al guardar preferencias.');
             }
         });
     });
